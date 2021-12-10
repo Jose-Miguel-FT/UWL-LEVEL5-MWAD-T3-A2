93a997c9c25260f9e7127240a6f090e89c7b94ea
Jose-Miguel-FT <miguelfinol_48@hotmail.com>
2021-12-10 20:07:10 +0000

    1 file modified

--- src/components/MedicalTreatmentPanel.js
+++ src/components/MedicalTreatmentPanel.js
@@ -2,15 +2,10 @@ import React from 'react';
 
 function MedicalTreatmentPanel() {
   const [treatId, setTreatId] = React.useState('');
-
   const [treatCourseId, setTreatCourseId] = React.useState('');
-
   const [type, setType] = React.useState('');
-
   const [category, setCategory] = React.useState('');
-
   const [name, setName] = React.useState('');
-
   const [startDate, setStartDate] = React.useState('');
 
   return (
